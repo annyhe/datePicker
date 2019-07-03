4c55abe5aca6d95ab79accb28eecb13ca443c163
Anny He <anny.he@salesforce.com>
2019-07-03 15:14:30 -0700

    initial commit with momentjs

---
+++ src/modules/my/calendarWithMoment/calendarWithMoment.js
@@ -0,0 +1,82 @@
+import { LightningElement, track } from 'lwc';
+import moment from 'moment';
+
+export default class CalendarWithMoment extends LightningElement {
+    initializeDatepicker;
+    @track calendarString;
+    today = moment();
+    dateContext = moment();
+    selectedDate = moment();
+    days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+    get year() {
+        return this.dateContext.format('Y');
+    }
+    get month() {
+        return this.dateContext.format('MMMM');
+    }
+    get currentDate() {
+        return this.dateContext.get('date');
+    }
+
+    previousMonth() {
+        moment(this.dateContext).subtract(1, 'month');
+    }
+
+    nextMonth() {
+        moment(this.dateContext).add(1, 'month');
+    }
+
+    goToday() {
+        this.selectedDate = this.today;
+        this.dateContext = this.today;
+        this.drawCalendar();
+    }
+
+    drawCalendar() {
+        const calendarHolder = this.template.querySelector('.calendarHolder');
+        const calendar = [];
+        const startWeek = moment()
+            .startOf('month')
+            .week();
+        const endWeek = moment()
+            .endOf('month')
+            .week();
+
+        for (let week = startWeek; week <= endWeek; week++) {
+            Array(7)
+                .fill(0)
+                .forEach((n, i) => {
+                    const day = moment()
+                        .week(week)
+                        .startOf('week')
+                        .clone()
+                        .add(n + i, 'day');
+                    const listElem = document.createElement('li');
+                    if (day.month() === this.dateContext.month()) {
+                        listElem.setAttribute('class', 'date');
+                        listElem.setAttribute(
+                            'data-date',
+                            day.format('YYYY-MM-DD')
+                        );
+                    } else {
+                        listElem.setAttribute('class', 'padder');
+                    }
+                    listElem.textContent = day.format('DD');
+                    calendarHolder.appendChild(listElem);
+                });
+        }
+
+        this.calendarString = JSON.stringify(calendar);
+    }
+
+    // use data-date in HTML, versus build out the selected date?
+    renderedCallback() {
+        if (this.initializeDatepicker) {
+            return;
+        }
+
+        this.initializeDatepicker = true; 
+        this.drawCalendar();
+    }
+}
